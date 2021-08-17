eae2fd16777694574ad9d0b4a446c11606c48e50
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-17 11:23:46 +0530

    Added: Choose symbol X or O

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,6 +1,11 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+
+    public static char playerSymbol;
+    public static char cpuSymbol;
 
     public static char[] createBoard() {
         char[] board = new char[10]; //board size of 10
@@ -11,7 +16,27 @@ public class TicTacToeGame {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game");
-        createBoard();
+        char[] board = createBoard();
+        System.out.println("Select the Letter you wanted \n  Press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
+        Scanner scan =new Scanner(System.in);
+        int choice = scan.nextInt();
+        switch(choice){
+            case 1 : {
+                playerSymbol='X';
+                cpuSymbol='O';
+                break;
+            }
+            case 2 :{
+                playerSymbol='O';
+                cpuSymbol='X';
+                break;
+            }
+            default: {
+                System.out.println("Invalid Choice . Retry Again!");
+                return;
+            }
+
+        }
     }
+
 }
