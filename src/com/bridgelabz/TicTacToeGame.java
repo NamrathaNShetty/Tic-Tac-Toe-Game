6b2ed295627767620f4eca4d509d8aef2ae6c1d5
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-17 12:05:46 +0530

    Added: Move to desired location

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -7,6 +7,8 @@ public class TicTacToeGame {
     //Variables
     public static char playerSymbol;
     public static char cpuSymbol;
+    public static int playerPos;
+    public static int cpuPos;
     static char[] board = new char[10]; //board size of 10
 
     public static char[] createBoard() {
@@ -46,6 +48,25 @@ public class TicTacToeGame {
                 return;
             }
         }
+        TicTacToeGame.showBoard();
+
+        //Select the position in the board
+        System.out.println("\nSelect the position to in board from range [ 1 - 9 ] :");
+        playerPos= scan.nextInt();
+
+        switch(playerPos) {
+            case 0 : {if(board[1]==' ') { board[0]=playerSymbol;} break;}
+            case 1 : {if(board[2]==' ') { board[1]=playerSymbol;} break;}
+            case 2 : {if(board[3]==' ') { board[2]=playerSymbol;} break;}
+            case 3 : {if(board[4]==' ') { board[3]=playerSymbol;} break;}
+            case 4 : {if(board[5]==' ') { board[4]=playerSymbol;} break;}
+            case 5 : {if(board[6]==' ') { board[5]=playerSymbol;} break;}
+            case 6 : {if(board[7]==' ') { board[6]=playerSymbol;} break;}
+            case 7 : {if(board[8]==' ') { board[7]=playerSymbol;} break;}
+            case 8 : {if(board[9]==' ') { board[8]=playerSymbol;} break;}
+            default : System.out.println("Invalid Position ! Restart game"); return;
+        }
+
         TicTacToeGame.showBoard();
 
     }
