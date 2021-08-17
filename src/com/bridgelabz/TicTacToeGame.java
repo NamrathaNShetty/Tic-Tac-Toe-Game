12abf7a2f111b0bfdc30e6a8b79d01d25a7f21bb
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-17 11:46:28 +0530

    Added: Display board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -4,21 +4,31 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
+    //Variables
     public static char playerSymbol;
     public static char cpuSymbol;
+    static char[] board = new char[10]; //board size of 10
 
     public static char[] createBoard() {
-        char[] board = new char[10]; //board size of 10
         for (int i = 0; i < board.length; i++) { //Iteration from 1 to 9
             board[i] = ' '; //initializing empty values
         }
         return board;
     }
 
+       //Display the board
+        public static void showBoard() {
+            System.out.println("Board Display\n");
+            System.out.println(" | "+board[1]+" "+board[2]+" "+board[3]+" |\n");
+            System.out.println(" | "+board[4]+" "+board[5]+" "+board[6]+" |\n");
+            System.out.println(" | "+board[7]+" "+board[8]+" "+board[9]+" |\n");
+    }
+
+    //Function to choose symbol X or O
     public static void main(String[] args) {
-        char[] board = createBoard();
+       createBoard();
         System.out.println("Select the Letter you wanted \n  Press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
-        Scanner scan =new Scanner(System.in);
+        Scanner scan =new Scanner(System.in); //Create scanner object
         int choice = scan.nextInt();
         switch(choice){
             case 1 : {
@@ -35,8 +45,9 @@ public class TicTacToeGame {
                 System.out.println("Invalid Choice . Retry Again!");
                 return;
             }
-
         }
+        TicTacToeGame.showBoard();
+
     }
 
 }
